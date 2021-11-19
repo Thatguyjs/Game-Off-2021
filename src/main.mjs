f946b345f11d7c4c05a4b9ee6a062d224dbac213
Thatguyjs <that.program@gmail.com>
2021-11-18 17:04:30 -0700

    Basic rendering done

--- src/main.mjs
+++ src/main.mjs
@@ -10,4 +10,72 @@ const program = twgl.createProgramInfo(
 	]
 );
 
-console.log(program);
+const attribs = {
+	position: { numComponents: 2, data: [
+		10, 10,
+		30, 10,
+		10, 30,
+		30, 30
+	]},
+
+	color: [
+		1, 0, 0, 1,
+		1, 0, 1, 1,
+		0, 1, 0, 1,
+		0, 0, 1, 1
+	],
+
+	indices: [
+		0, 1, 2,
+		1, 2, 3
+	]
+};
+
+let buffers = twgl.createBufferInfoFromArrays(gl, attribs);
+
+const uniforms = {
+	viewport: [window.innerWidth, window.innerHeight],
+	world_mat: twgl.m4.identity(),
+	model_mat: twgl.m4.identity()
+};
+
+
+gl.clearColor(0.063, 0.063, 0.063, 1);
+gl.enable(gl.DEPTH_TEST);
+gl.enable(gl.BLEND);
+gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+
+function render() {
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	buffers = twgl.createBufferInfoFromArrays(gl, attribs);
+
+	gl.viewport(0, 0, canvas.width, canvas.height);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+	gl.useProgram(program.program);
+	twgl.setBuffersAndAttributes(gl, program, buffers);
+	twgl.setUniforms(program, uniforms);
+
+	twgl.drawBufferInfo(gl, gl.TRIANGLES, buffers);
+
+	window.requestAnimationFrame(render);
+}
+
+window.requestAnimationFrame(render);
+
+
+window.onmousemove = (ev) => {
+	attribs.position.data[0] = ev.clientX;
+	attribs.position.data[1] = ev.clientY;
+
+	attribs.position.data[2] = ev.clientX + 20;
+	attribs.position.data[3] = ev.clientY;
+
+	attribs.position.data[4] = ev.clientX;
+	attribs.position.data[5] = ev.clientY + 20;
+
+	attribs.position.data[6] = ev.clientX + 20;
+	attribs.position.data[7] = ev.clientY + 20;
+}
--- src/shaders/color.vert
+++ src/shaders/color.vert
@@ -2,15 +2,25 @@
 
 precision highp float;
 
-in vec4 position;
+in vec2 position;
 in vec4 color;
 
+uniform vec2 viewport;
 uniform mat4 world_mat;
 uniform mat4 model_mat;
 
 out vec4 frag_color;
 
+vec2 screen_to_gl_coord(vec2 pos) {
+	vec2 half_viewport = viewport / 2.0;
+
+	return vec2(
+		(pos.x - half_viewport.x) / half_viewport.x,
+		(half_viewport.y - pos.y) / half_viewport.y
+	);
+}
+
 void main() {
-	gl_Position = world_mat * model_mat * position;
+	gl_Position = world_mat * model_mat * vec4(screen_to_gl_coord(position), 0.0, 1.0);
 	frag_color = color;
 }
